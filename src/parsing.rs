956b40eb9be2213c0d629d6ab92135ee7717eb42
CarbonFlora <zihaoliang0413@gmail.com>
2023-08-22 15:44:52 -0700

    working on new longform

--- src/parsing.rs
+++ src/parsing.rs
@@ -1,9 +1,21 @@
 use anyhow::Result;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 use crate::dictionary::KeyPair;
 
-pub fn new_longform(path: &str) -> Result<KeyPair> {
+pub fn new_longform(file_path: &str) -> Result<KeyPair> {
     let map = KeyPair::new();
+    let buffered = BufReader::new(File::open(file_path)?)
+        .lines()
+        .flatten()
+        .peekable();
+
+    for line in buffered {
+        if line.starts_with(':') {
+            let left = line.split_whitespace()
+        }
+    }
 
     Ok(map)
 }
@@ -15,5 +27,10 @@ pub fn build_longform() -> Result<KeyPair> {
 }
 
 pub fn key_swap(keypair: KeyPair) -> KeyPair {
-    keypair
+    let mut rev_keypair = KeyPair::new();
+    for pair in keypair.iter() {
+        rev_keypair.insert(pair.1.to_string(), pair.0.to_string());
+    }
+
+    rev_keypair
 }
